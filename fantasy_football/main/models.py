410e1d2f9b8f3d47adf3c296477fde6e9fa38081
thutch17 <tatethutchins@gmail.com>
2025-03-29 00:42:48 -0400

    database users, leagues, matchups

--- fantasy_football/main/models.py
+++ fantasy_football/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -21,3 +22,22 @@ class Stock(models.Model):
 
     def __str__(self):
         return self.ticker
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_address = models.CharField(max_length=30)
+    team_name = models.CharField(max_length=30)
+    user_stocks = models.ManyToManyField('Stock', related_name="users")
+
+class League(models.Model):
+    weeks = models.IntegerField(max_digits=3)
+    current_week = models.IntegerField(max_digits=3)
+
+class Matchup(models.Model):
+    league = models.ForeignKey('League', on_delete=models.CASCADE)
+    week = models.IntegerField(max_digits=3)
+    first_user = models.ForeignKey('UserProfile', related_name="matchups")
+    second_user = models.ForeignKey('UserProfile', related_name="matchups")
+    # 0 not finished, 1 first user won, 2 second user won, 3 tie
+    winner = models.IntegerField(max_digits=1, default=0)
+
